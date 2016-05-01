46a5268f692fec52e8075db540dbe237ba23a41d
kady <elkadyabdelrahman@gmail.com>
2016-05-01 21:44:11 +0200

    Use Grunt for compilation and cleaning

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    shell: {
+      options: {
+        // Run in thesis/ dir and ignore STDERR
+        stderr: false,
+        execOptions: {
+          cwd: 'thesis'
+        }
+      },
+
+      compile: {
+        command: 'make'
+      },
+
+      clean: {
+        command: 'make clean'
+      }
+
+    },
+
+    watch: {
+      files: ['thesis/bachelor.tex'],
+      tasks: ['shell:compile', 'shell:clean'],
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+
+};
